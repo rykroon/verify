732cff570af41afe29e169e790e31a3ee59f0316
Ryan Kroon <rykroon.tech@gmail.com>
2025-08-05 21:35:16 -0400

    initial http server logic.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/rykroon/verify/internal/server"
+)
+
+func main() {
+	jsonRpcServer := server.GetJsonRpcServer()
+	http.Handle("/jsonrpc", jsonRpcServer)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println(err)
+	}
 }
